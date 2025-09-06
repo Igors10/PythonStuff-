a31124858a472a290c472586257430d485d58ccb
Alkam98 <alkamts98@gmail.com>
2025-09-06 12:37:24 +0200

    corrected some error

--- taskmanager.py
+++ taskmanager.py
@@ -2,7 +2,7 @@
 # Desired functions- Store Tasks (Task has description, deadline, difficulty rating, done status), Add tasks, Review Tasks, Remove Tasks, Change tasks
 
 class Task:  # Hello Mallo
-    def AddTask(self, name, : str, desc: str, deadline: str, difficulty: int, status: bool)
+    def AddTask(self, name, str, desc: str, deadline: str, difficulty: int, status: bool)
     # Add task function
     self.desc = desc
     self.deadline = deadline
@@ -10,18 +10,17 @@ class Task:  # Hello Mallo
     self.status = status
     self.name = name
 
-    def RemoveTask(nameToRemove: str)
-    # Removes the task from the list
+    def RemoveTask(nameToRemove: str):
+        # Removes the task from the list
 
-    def ReviewTask(nameToReview: str)
-    # Types out the task
+    def ReviewTask(nameToReview: str):
+        # Types out the task
 
-    def ChangeTask(nameToChange: str, attributeToChange: str, newAttribute)
-    # Changes a specific attribute of a task
+    def ChangeTask(nameToChange: str, attributeToChange: str, newAttribute):
+        # Changes a specific attribute of a task
 
-
-def main():
-    print("Welcome to task manager X3000")
+        def main():
+            print("Welcome to task manager X3000")
 
 
 main()
